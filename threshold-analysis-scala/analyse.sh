6849e83fb92f0774e4bffada554a4c1faafc55ee
Phillip Taylor <phillip.taylor@transformuk.com>
2023-09-12 20:41:29 +0100

    Remove old scala analysis script

--- threshold-analysis-scala/analyse.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run -it --rm -v "$AI_EDOCUMENT_DETECTOR_ROOT/threshold-analysis-scala:/tmp/app" -v "$TRAINING_SET_ROOT:/tmp/images" -v "$MODEL_PATH:/tmp/saved-model" -w /tmp -u 1000:1000 tensorflow-edocument java -jar /tmp/app/target/scala-3.2.2/scala-client.jar
