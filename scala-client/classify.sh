92a7f9e5af8b63a3b1e9d417a5c09f8a1274dd7d
Phillip Taylor <phillip.taylor@transformuk.com>
2023-09-12 11:19:15 +0100

    Switch the classification script to use GPU for training

--- scala-client/classify.sh
+++ scala-client/classify.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it --rm -v "$AI_EDOCUMENT_DETECTOR_ROOT/scala-client:/tmp/app" -v "$TRAINING_SET_ROOT:/tmp/images" -v "$MODEL_PATH:/tmp/saved-model" -w /tmp -u 1000:1000 tensorflow-edocument java -jar /tmp/app/target/scala-3.2.2/scala-client.jar
+docker run -it --rm --gpus all --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 -v "$AI_EDOCUMENT_DETECTOR_ROOT/scala-client:/tmp/app" -v "$TEST_SET_ROOT:/tmp/images" -v "$MODEL_PATH:/tmp/saved-model" -e MODEL_PATH -e TEST_SET_ROOT -w /tmp -u 1000:1000 ai-edocument-detector java -jar /tmp/app/target/scala-3.2.2/scala-client.jar
